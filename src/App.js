1780178b6afd58a4994643aaaf771212a2665784
Patrick Kim <patrick.yrkim@gmail.com>
2022-08-16 23:49:56 -0700

    Add omdbapi and hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
+const API_URL = OMDB_API_URL
+
 const App = () => {
+    const searchMovies = async () => {
+        const res = await fetch(`${API_URL}&s=${title}`)
+        const data = await res.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies("The Emperor's New Groove")
+    }, [])
+
+    // console.log(API_URL)
     return (
         <h1>
             App
